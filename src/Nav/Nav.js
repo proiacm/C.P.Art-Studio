15a9fa6c2f1836d94ae68161a8ad93a6b33d2f61
proiacm <proiacm@gmail.com>
2020-12-02 13:26:18 -0700

    Initial Nav layout

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -4,8 +4,12 @@ import './Nav.css';
 const Nav = () => {
 
   return (
-      <div>
-          <h1>Nav</h1>
+      <div className='nav'>
+          <p className='nav-li'>About</p>
+          <p className='nav-li'>The Studio</p>
+          <p className='nav-li'>Gallery</p>
+          <p className='nav-li'>Testimonials</p>
+          <p className='nav-li'>Contact</p>
       </div>
   )
 }
