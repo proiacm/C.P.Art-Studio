4f652e3b7a7cc633f8f42bb98fa278de66419f61
proiacm <proiacm@gmail.com>
2020-12-02 13:01:55 -0700

    Set up Nav component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, {Component} from 'react'
 import './App.css';
+import Nav from './Nav/Nav';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>We're up and running!</h1>
-        </header>
+        <Nav />
       </div>
     );
   }
