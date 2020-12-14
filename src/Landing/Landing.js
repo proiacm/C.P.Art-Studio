64fdca1a198fd008a0dbd3029cf779186c5770f1
proiacm <proiacm@gmail.com>
2020-12-14 15:39:08 -0700

    Update Landing text

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './Landing.css';
+import './Landing.scss';
 
 const Landing = () => {
 
   return (
-        <div className='landing' id='landing'>
-          Landing Area
+        <div className='landing'>
+          <h1 className='landing-title'>C.P. Art Studio</h1>
+          <h2 className='landing-text'>Tagline goes here</h2>
         </div>
   )
 }
