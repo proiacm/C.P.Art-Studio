7de3f73b9baa48838582f189b3f7ee1011b56e9d
proiacm <proiacm@gmail.com>
2020-12-14 16:32:00 -0700

    Remove <u> from component and add text-decoration to css

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -6,11 +6,11 @@ const Nav = () => {
 
   return (
       <div className='nav'>
-          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}><u>About</u></Link></p>
-          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="studio" spy={true} smooth={true}><u>The Studio</u></Link></p>
-          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="gallery" spy={true} smooth={true}><u>Gallery</u></Link></p>
-          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="testimonials" spy={true} smooth={true}><u>Testimonials</u></Link></p>
-          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="contact" spy={true} smooth={true}><u>Contact</u></Link></p>
+          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}>About</Link></p>
+          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="studio" spy={true} smooth={true}>The Studio</Link></p>
+          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="gallery" spy={true} smooth={true}>Gallery</Link></p>
+          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="testimonials" spy={true} smooth={true}>Testimonials</Link></p>
+          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="contact" spy={true} smooth={true}>Contact</Link></p>
       </div>
   )
 }
