af24a68bf416847cfbac08390d419e9fb0573a7c
proiacm <proiacm@gmail.com>
2020-12-19 10:09:48 -0700

    Update Nav

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -6,7 +6,7 @@ const Nav = () => {
 
   return (
       <div className='nav'>
-          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}>About</Link></p>
+          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}>About Me</Link></p>
           <p className='nav-li'><Link style={{ cursor: "pointer"}} to="studio" spy={true} smooth={true}>The Studio</Link></p>
           <p className='nav-li'><Link style={{ cursor: "pointer"}} to="gallery" spy={true} smooth={true}>Gallery</Link></p>
           <p className='nav-li'><Link style={{ cursor: "pointer"}} to="testimonials" spy={true} smooth={true}>Testimonials</Link></p>
