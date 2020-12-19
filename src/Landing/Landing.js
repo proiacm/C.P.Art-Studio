2c07bc4d369032bd64cd771dfb27d8ff19c75b70
proiacm <proiacm@gmail.com>
2020-12-19 10:09:27 -0700

    Update landing

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Landing.scss';
+// import logo from '../Images/cplogo.png';
+import logo from '../Videos/animatedlogo.mp4';
 
 const Landing = () => {
 
   return (
         <div className='landing'>
-          <h1 className='landing-title'>C.P. Art Studio</h1>
-          <h2 className='landing-text'>Tagline goes here</h2>
+          {/* <img className='logo' src={logo} alt='logo' /> */}
+          <video className='logo' autoPlay muted >
+          <source src={logo} type="video/mp4"/>
+          </video>
         </div>
   )
 }
