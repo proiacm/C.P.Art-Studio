fd3bcb27df2ba9bf6cbe9f20eb9f5b28dad9edab
proiacm <proiacm@gmail.com>
2020-12-19 10:09:17 -0700

    Create form

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './Contact.css';
 
-const Contact = () => {
+const Contact = (props) => {
 
   return (
         <div className='contact' id='contact'>
-          Contact Area
-        </div>
+          <div className='form'>
+            <form onSubmit={props.submit}>
+              <label className='form-label'>
+                FULL NAME <br/>
+                <input className='input' type="text" name="name" placeholder='Enter your full name' onChange={props.nameChanged}/>
+              </label><br/><br/>
+              <label className='form-label'>
+                PHONE <br/>
+                <input className='input' type="text" name="phone" placeholder='Enter your phone number' onChange={props.phoneChanged} />
+              </label><br/><br/>
+              <label className='form-label'>
+                EMAIL <br/>
+                <input className='input' type="text" name="email" placeholder='Enter your email' onChange={props.emailChanged} />
+              </label><br/><br/>
+              <label className='form-label'>
+                MESSAGE <br/>
+                <input className='input-text' type="text" name="message" placeholder='Leave me a note' onChange={props.messageChanged} />
+              </label> <br/><br/>
+              <input className='submit' type="submit" value="Send Message" />
+            </form>
+          </div>
+    </div>
   )
 }
 
