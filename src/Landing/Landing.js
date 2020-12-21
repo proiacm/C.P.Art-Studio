de822ea07fa33c6e426c97fc7c47d93c4d57e3f9
proiacm <proiacm@gmail.com>
2020-12-20 22:15:54 -0700

    Add id for react-scroll

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -6,7 +6,7 @@ import logo from '../Videos/animatedlogo.mp4';
 const Landing = () => {
 
   return (
-        <div className='landing'>
+        <div className='landing' id='home'>
           {/* <img className='logo' src={logo} alt='logo' /> */}
           <video className='logo' autoPlay muted >
           <source src={logo} type="video/mp4"/>
