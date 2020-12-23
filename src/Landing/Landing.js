4a6885fed3ccd6892b7545fffc4a4b2711934d35
proiacm <proiacm@gmail.com>
2020-12-22 19:16:25 -0700

    Animate and style first element

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import './Landing.scss';
-// import logo from '../Images/cplogo.png';
-import logo from '../Videos/animatedlogo.mp4';
+import './Landing.css';
+// import logo from '../Videos/animatedlogo.mp4';
 
 const Landing = () => {
 
   return (
         <div className='landing' id='home'>
-          {/* <img className='logo' src={logo} alt='logo' /> */}
-          <video className='logo' autoPlay muted >
+          {/* <video className='logo' autoPlay muted >
           <source src={logo} type="video/mp4"/>
-          </video>
+          </video> */}
+          <p className='cp'>CP</p>
         </div>
   )
 }
